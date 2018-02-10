1180953a06e5e1668da7d7de48bf8c5c10d5ce5a
Miki Maine <mikias.amdu@gmail.com>
2018-02-10 14:36:20 +0300

    todo components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SampleComponent } from './sample/sample.component';
+import { AboutComponent } from './about/about.component';
+import { NavComponent } from './nav/nav.component';
+import { TodosComponent } from './todos/todos.component';
+import { TodoComponent } from './todo/todo.component';
+import { NewTodoComponent } from './new-todo/new-todo.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleComponent,
+    AboutComponent,
+    NavComponent,
+    TodosComponent,
+    TodoComponent,
+    NewTodoComponent
   ],
   imports: [
     BrowserModule
